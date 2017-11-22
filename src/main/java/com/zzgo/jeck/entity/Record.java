ff3a1a8a30590872f94e5e55ae3fd29d1059d784
815609229@qq.com <815609229@qq.com>
2017-11-22 11:35:00 +0800

    wwww5.1

---
+++ src/main/java/com/zzgo/jeck/entity/Record.java
@@ -0,0 +1,57 @@
+package com.zzgo.jeck.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by 9527 on 2017/11/22.
+ */
+@Entity
+@Table(name = "tab_record")
+public class Record {
+    private String id;
+    private String name;
+    private String situation;
+    private Date recordTime;
+
+    @Id
+    @GenericGenerator(name = "uuid", strategy = "assigned")
+    @GeneratedValue(generator = "uuid")
+    @Column(name = "record_id", unique = true, nullable = false)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(name = "record_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "record_situation")
+    public String getSituation() {
+        return situation;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    @Column(name = "record_record_time")
+    public Date getRecordTime() {
+        return recordTime;
+    }
+
+    public void setRecordTime(Date recordTime) {
+        this.recordTime = recordTime;
+    }
+}
